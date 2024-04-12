d94da4114d5d8ffbad110c7cb2b1441a302020d1
surelle-ha <0110harold@gmail.com>
2024-04-12 22:05:17 +0800

    add fix

--- vue.config.js
+++ vue.config.js
@@ -2,8 +2,8 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig(
   {
     transpileDependencies: true,
-    devServer: {
-      disableHostCheck: true
-    }
+      devServer: {
+        allowedHosts: "all"
+      }
   }
 )
