b5352c9ba4ee0f70a124b2090e72a1dcaa202f00
surelle-ha <0110harold@gmail.com>
2024-04-12 22:00:32 +0800

    deploy fixed

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,9 @@
 const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+module.exports = defineConfig(
+  {
+    transpileDependencies: true,
+    devServer: {
+      disableHostCheck: true
+    }
+  }
+)
